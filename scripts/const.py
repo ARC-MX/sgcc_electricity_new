77487e6891d365d4a00872c0bd6d2009402d4e3e
2ndMessiah <lym.0072003@gmail.com>
2025-07-14 11:32:47 +0800

    Update const.py
    修改95598的url

--- scripts/const.py
+++ scripts/const.py
@@ -1,8 +1,8 @@
 # 填写普通参数 不要填写密码等敏感信息
 # 国网电力官网
-LOGIN_URL = "https://www.95598.cn/osgweb/login"
-ELECTRIC_USAGE_URL = "https://www.95598.cn/osgweb/electricityCharge"
-BALANCE_URL = "https://www.95598.cn/osgweb/userAcc"
+LOGIN_URL = "https://95598.cn/osgweb/login"
+ELECTRIC_USAGE_URL = "https://95598.cn/osgweb/electricityCharge"
+BALANCE_URL = "https://95598.cn/osgweb/userAcc"
 
 
 # Home Assistant
